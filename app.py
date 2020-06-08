e533ea09fc9144680273c3f87799bfc3dc006791
Rellikdog <richard.bellamy123@gmail.com>
2020-06-08 11:18:56 -0700

    front end mvp

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #=================================================================================================================================
 #                                               Imports
 #=================================================================================================================================
+from flask import Flask, request, render_template, jsonify
 from src.api_calls import get_match_history, get_match_details
-from src.cleaners import id_list_from_history, clean_match_details, make_csv
+from src.cleaners import id_list_from_history, clean_match_details, make_csv, make_pred_row
 from src.populate_db import populate_db
 import requests
 import pymongo
@@ -12,10 +13,11 @@ db = client.dota_data
 from src.heroes import heroes, name_id, id_name
 from src.eda import make_counter
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from src.models import train_rfc
+from src.models import train_rfc, rfc_pred
+import numpy as np
 import pickle
+app = Flask(__name__)
 #=================================================================================================================================
 #                                                     Routes
 #=================================================================================================================================
@@ -43,9 +45,8 @@ def solve():
     return jsonify({'prediction': prediction})
 
 def pred(a, b):
-    
-    print(a, b)
-    return 'Team A wins!'
+    pred_row = make_pred_row(pd.read_csv('test.csv'), a, b)
+    return rfc_pred(pred_row)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
